1dd82d9b800f5fbb8e4bebb3e646de6f53e85324
AwaisKhan765 <80539241+AwaisKhan765@users.noreply.github.com>
2021-12-27 23:00:59 +0500

    Add files via upload

---
+++ Service_On_Boot/app/src/main/java/com/example/service_on_boot/StartAppOnBoot.kt
@@ -0,0 +1,13 @@
+package com.example.service_on_boot
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val intent1 = Intent(this@MainActivity, RunServiceOnBoot::class.java)
+        startService(intent1)
+    }
+}
